385184306871d4102893e521d467de7ddca34b7e
Henrique Abreu <henriqueg@ciandt.com>
2018-09-17 14:04:24 -0500

    added network vpc config/module

--- main.tf
+++ main.tf
@@ -13,3 +13,34 @@ module "project-factory" {
   credentials_path    = "/home/henriqueg/.config/gcloud/application_default_credentials.json"
 }
 
+module "vpc" {
+  source = "../terraform-google-network"
+  project_id = "cit-henriqueg-tf-test-33d7"
+  network_name = "myvpc"
+
+  subnets = [
+    {
+      subnet_name           = "subnet-01"
+      subnet_ip             = "10.10.10.0/24"
+      subnet_region         = "us-west1"
+    },
+    {
+      subnet_name           = "subnet-02"
+      subnet_ip             = "10.10.20.0/24"
+      subnet_region         = "us-west1"
+      subnet_private_access = true
+      subnet_flow_logs      = true
+    },
+  ]
+
+  secondary_ranges = {
+    subnet-01 = [
+      {
+        range_name    = "subnet-01-secondary-01"
+        ip_cidr_range = "192.168.64.0/24"
+      },
+    ]
+
+    subnet-02 = []
+  }
+}
