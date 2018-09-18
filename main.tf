b20e18a3e5a5b554cecafc24f97ddec537b59d09
Henrique Abreu <henriqueg@ciandt.com>
2018-09-18 10:08:21 -0500

    include log-export example

--- main.tf
+++ main.tf
@@ -1,35 +1,32 @@
-provider "google" {
-  project = "kohls-ciandt-1"
-}
-
 #deleted gsuite_override.tf file in project-factory source folder
 
-module "project-factory" {
+module "projectf" {
   source              = "../terraform-google-project-factory"
   name                = "cit-henriqueg-tf-test"
   random_project_id   = "true"
   org_id              = "1734241926"
   billing_account     = "00FEAB-036EA4-E2DEF7"
   credentials_path    = "/home/henriqueg/.config/gcloud/application_default_credentials.json"
+
+  activate_apis = [
+    "compute.googleapis.com",
+  ]
+}
+
+output "pid" {
+  value = "${module.projectf.project_id}"
 }
 
 module "vpc" {
   source = "../terraform-google-network"
-  project_id = "cit-henriqueg-tf-test-33d7"
-  network_name = "myvpc"
+  project_id = "${module.projectf.project_id}"
+  network_name = "myvpc3"
 
   subnets = [
     {
       subnet_name           = "subnet-01"
       subnet_ip             = "10.10.10.0/24"
       subnet_region         = "us-west1"
-    },
-    {
-      subnet_name           = "subnet-02"
-      subnet_ip             = "10.10.20.0/24"
-      subnet_region         = "us-west1"
-      subnet_private_access = true
-      subnet_flow_logs      = true
     },
   ]
 
@@ -40,7 +37,15 @@ module "vpc" {
         ip_cidr_range = "192.168.64.0/24"
       },
     ]
+  }
+}
 
-    subnet-02 = []
+module "logs" {
+  source = "../terraform-google-log-export"
+  name = "mylogexport"
+  project = "${module.projectf.project_id}"
+  bigquery = {
+    name = "isthisdatasetname"
+    project = "kohls-ciandt-1"
   }
 }
